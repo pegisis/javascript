d267e54e3256fa148bb0899f7aa58e2608658162
yash meena <58389153+pegisis@users.noreply.github.com>
2025-06-05 09:05:10 +0000

    #

---
+++ Basics/01_Variables.js
@@ -0,0 +1,6 @@
+const customerId=475892
+let name = "yash"
+var type = "developer" 
+
+name = "pegisis"
+console.table([customerId,name,type])
---
+++ Basics/02_DataTypes.js
@@ -0,0 +1,10 @@
+let name = "yash"
+let age = 56
+let boolean = true 
+let sex = undefined
+let phone_no= null
+
+
+
+console.log(typeof(age));
+
