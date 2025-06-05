e23ac260283b032a2d0b541879000d0b42c770d1
yash meena <58389153+pegisis@users.noreply.github.com>
2025-06-05 09:56:59 +0000

    #

--- Basics/03_OperationConversion.js
+++ Basics/03_OperationConversion.js
@@ -1,16 +1,41 @@
 let score = "33aaa"
 
-console.log(typeof score);
+//console.log(typeof score);
 
-console.log(typeof (score));
+//console.log(typeof (score));
 
 
 let ValueInNumber= Number(score)
 
-console.log(typeof (ValueInNumber));
+//console.log(typeof (ValueInNumber));
 
-console.log( ValueInNumber);
+//console.log( ValueInNumber);
 
 
 // "33"==> number
 // "33nan"==> NaN not a number
+
+
+
+
+// **********************OPERATIONS*************************************
+
+
+let str1= "hello"
+
+let str2 = " yash"
+
+let str3= str1+str2
+
+console.log(str3);
+
+
+console.log(1+2)
+
+console.log(1+"2")
+
+console.log("1"+2)
+
+console.log("1"+2+5)
+
+console.log(1+2+"6")
