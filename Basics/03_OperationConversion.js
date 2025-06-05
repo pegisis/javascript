4f5381b922f77b20481c080cde873f0b46f76093
yash meena <58389153+pegisis@users.noreply.github.com>
2025-06-05 09:20:09 +0000

    converstion of datatype

---
+++ Basics/03_OperationConversion.js
@@ -0,0 +1,16 @@
+let score = "33aaa"
+
+console.log(typeof score);
+
+console.log(typeof (score));
+
+
+let ValueInNumber= Number(score)
+
+console.log(typeof (ValueInNumber));
+
+console.log( ValueInNumber);
+
+
+// "33"==> number
+// "33nan"==> NaN not a number
